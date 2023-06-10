774411ac284c948f6e2a5a4ff34e0135601cc787
Joniclei <jonicleibernardojunior@gmail.com>
2023-06-10 18:32:20 -0300

    ma

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Matrixx
+{
+  class Program
+  {
+    public static void Main(string[]args)
+    {
+      int [,] matriX = new int[];
+      Console.Write("");
+      int linha = Convert.ToInt32(Console.ReadLine());
+      Console.Write("");
+      int coluna = Convert.ToInt32(Console.ReadLine());
+
+      for(int l = 0; l<linha ; l++){
+
+        for()
+      }
+
+    }
+  }
+}
